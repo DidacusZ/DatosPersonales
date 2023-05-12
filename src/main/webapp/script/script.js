8e436914ffd6b9343cab55d83022f8af0e3643b0
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-05-12 13:38:10 +0200

    version recuperacion

---
+++ src/main/webapp/script/script.js
@@ -0,0 +1,21 @@
+function guardarDatos(){
+		
+let edad = document.getElementsByName('edad')[0].value;
+let nombre = document.getElementsByName('nombre')[0].value;
+let apellidos = document.getElementsByName('apellidos')[0].value;
+
+localStorage.setItem("nombre", nombre);
+localStorage.setItem("apellidos", apellidos);
+localStorage.setItem("edad", edad);
+		
+window.open("mostrarDatos.html")
+
+}
+
+function Mostrardatos(){
+
+document.write("Nombre: "+localStorage.getItem("nombre"))
+document.write("<br>Apellidos: "+localStorage.getItem("apellidos"))
+document.write("<br>Edad: "+localStorage.getItem("edad"))
+
+}
